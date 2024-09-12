cbf8f75c9eb62d668a84a691fdbeadb88ac0fc79
Vineet Maurya <101665909+Vineet369@users.noreply.github.com>
2024-09-12 22:20:34 +0530

    updated links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -27,7 +27,7 @@ const NavBar = () => {
             <a href="#" target='_blank'>
             <FaInstagram className='text-slate-700 dark:text-slate-300'/>
             </a>
-            <a href="https://drive.google.com/file/d/1oNFfzsinbcyW_7d3sMJyJuQr3QI9HRyD/view?usp=drive_link" target='_blank'>
+            <a href="https://docs.google.com/document/d/1cNjV8YkDDCy4dqmbcaVHvc33eeB4Ar-4rQPva0PyRpQ/edit?usp=drive_link" target='_blank'>
           <h2 className='text-lg dark:text-slate-300'>| My Resume</h2>
         </a>
         </div>
